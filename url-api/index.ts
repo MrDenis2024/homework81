4dec1a319a0a426072a2181cc0e27b3154b41b66
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-29 14:10:41 +0600

    Создана структура в index для запуска сервера

--- url-api/index.ts
+++ url-api/index.ts
@@ -1,2 +1,19 @@
+import express from "express";
+import config from './config';
+import cors from "cors";
+import linksRouter from './routers/links';
 
+const app = express();
+const port = 8000;
 
+app.use(express.json());
+app.use(cors(config.corsOptions));
+app.use('/links', linksRouter);
+
+const run = async () => {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+};
+
+run().catch(console.error);
